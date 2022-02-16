8790aa91dc81f73f880f48d2e734b518f9866271
Marcin Kowal (PGS Software) <mkowal@pgs-soft.com>
2022-02-16 11:46:15 +0100

    project entity class

---
+++ src/main/java/com/kanbantool/kanbantool/domain/Project.java
@@ -0,0 +1,39 @@
+package com.kanbantool.kanbantool.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String projectName;
+    private String projectIdentifier;
+    private String description;
+    private Date start_date;
+    private Date end_date;
+
+    private Date created_at;
+    private Date updated_at;
+
+    @PrePersist
+    protected void onCreate() {
+        this.created_at = new Date();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updated_at = new Date();
+    }
+}
