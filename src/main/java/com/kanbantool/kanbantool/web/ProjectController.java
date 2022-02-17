7c9f2ee8cae484226df85a55c58404074f5276cc
Marcin Kowal (PGS Software) <mkowal@pgs-soft.com>
2022-02-17 17:24:26 +0100

    added validation demendencies added json validation

--- src/main/java/com/kanbantool/kanbantool/web/ProjectController.java
+++ src/main/java/com/kanbantool/kanbantool/web/ProjectController.java
@@ -5,10 +5,15 @@ import com.kanbantool.kanbantool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/project")
@@ -18,7 +23,11 @@ public class ProjectController {
     private ProjectService projectService;
 
     @PostMapping("")
-    private ResponseEntity<Project> createNewProject(@RequestBody Project project) {
+    private ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
+
+        if (result.hasErrors()) {
+            return new ResponseEntity<List<FieldError>>(result.getFieldErrors(), HttpStatus.BAD_REQUEST);
+        }
         Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<>(project, HttpStatus.CREATED);
     }
