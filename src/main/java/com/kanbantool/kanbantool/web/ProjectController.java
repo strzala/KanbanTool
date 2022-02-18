56da34a6e8bc8089522aa1b818891a7b3c6ed82f
Marcin Kowal (PGS Software) <mkowal@pgs-soft.com>
2022-02-18 07:22:15 +0100

    moved validation method outside contoller

--- src/main/java/com/kanbantool/kanbantool/web/ProjectController.java
+++ src/main/java/com/kanbantool/kanbantool/web/ProjectController.java
@@ -1,19 +1,18 @@
 package com.kanbantool.kanbantool.web;
 
 import com.kanbantool.kanbantool.domain.Project;
+import com.kanbantool.kanbantool.services.MapValidationErrorService;
 import com.kanbantool.kanbantool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/project")
@@ -22,12 +21,14 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
+
     @PostMapping("")
     private ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
+        ResponseEntity<?> errorMap = mapValidationErrorService.mapValidationErrorService(result);
+        if (errorMap != null) return errorMap;
 
-        if (result.hasErrors()) {
-            return new ResponseEntity<List<FieldError>>(result.getFieldErrors(), HttpStatus.BAD_REQUEST);
-        }
         Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<>(project, HttpStatus.CREATED);
     }
